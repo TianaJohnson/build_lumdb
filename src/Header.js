c66c3ee9b55c967e8cd11556c89170785202960a
Tiana <Tiana.marie013@gmail.com>
2021-01-05 19:16:26 -0700

    header title causes header white space gap

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
         return (
             <div className="header-main">
 
-                Welcome
+                <h1 className="header-title">Welcome</h1>
 
             </div>
          )
