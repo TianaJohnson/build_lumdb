5250b503451868be1e5870e3d88538f4d832773b
Tiana <Tiana.marie013@gmail.com>
2021-01-07 19:22:38 -0700

    body

---
+++ src/Body.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+
+class Body extends Component {
+    render() {
+        return (
+            <div>
+                <input/>
+                
+            </div>
+         )
+    }
+}
+
+
+export default Body;
